232165c126fbd59625ee8c6c94b35b751073bc9b
Flan <flannyha@gmail.com>
2023-01-02 17:51:15 +0100

    switch to doubles for sampling

--- SoundfontStudies/envs_lfos.cpp
+++ SoundfontStudies/envs_lfos.cpp
@@ -4,44 +4,44 @@
 #include <corecrt_math.h>
 
 namespace Flan {
-    void EnvState::update(const EnvParams& env_params, const float dt, const bool correct_attack_phase)
+    void EnvState::update(const EnvParams& env_params, const double dt, const bool correct_attack_phase)
     {
         // Update volume envelope - the idea is that the adsr_timer goes from 1.0 - 0.0 for every state
-        switch (static_cast<envStage>(stage)) {
+        switch (static_cast<EnvStage>(stage)) {
         case delay:
-            stage = std::min(static_cast<float>(attack), stage + env_params.delay * dt);
-            value = -100.0f;
+            stage = std::min(static_cast<double>(attack), stage + env_params.delay * dt);
+            value = -100.0;
             break;
         case attack:
-            stage = std::min(static_cast<float>(hold), stage + env_params.attack * dt);
+            stage = std::min(static_cast<double>(hold), stage + env_params.attack * dt);
             if (correct_attack_phase)
-                value = 6 * log2f(stage - static_cast<float>(attack));
+                value = 6 * log2(stage - static_cast<double>(attack));
             else
-                value = -100.0f + 100 * (stage - static_cast<float>(attack));
+                value = -100.0 + 100 * (stage - static_cast<double>(attack));
             break;
         case hold:
-            stage = std::min(static_cast<float>(decay), stage + env_params.hold * dt);
-            value = 0.0f;
+            stage = std::min(static_cast<double>(decay), stage + env_params.hold * dt);
+            value = 0.0;
             break;
         case decay:
             value -= env_params.decay * dt;
             if (value < env_params.sustain) {
                 value = env_params.sustain;
-                stage = static_cast<float>(sustain);
+                stage = static_cast<double>(sustain);
             }
             break;
         case sustain:
             // This stage will stay until note_off
-            stage = static_cast<float>(sustain);
+            stage = static_cast<double>(sustain);
             value = env_params.sustain;
             break;
         case release:
             // This stage will continue until the volume is 0.0f
-            stage = static_cast<float>(release);
+            stage = static_cast<double>(release);
             value -= env_params.release * dt;
-            if (value <= -100.0f) {
-                value = -100.0f;
-                stage = static_cast<float>(off);
+            if (value <= -100.0) {
+                value = -100.0;
+                stage = static_cast<double>(off);
             }
             break;
         default:
@@ -49,24 +49,24 @@ namespace Flan {
         }
     }
 
-    void LfoState::update(const LfoParams& lfo_params, const float dt)
+    void LfoState::update(const LfoParams& lfo_params, const double dt)
     {
         // Step time
         time += dt;
 
         // Handle delay
         if (time < lfo_params.delay) {
-            state = 0.0f;
+            state = 0.0;
         }
         else {
-            state = sinf((time - lfo_params.delay) * lfo_params.freq * 2.0f * 3.14159265359f);
+            state = sin((time - lfo_params.delay) * lfo_params.freq * 2.0 * 3.141592653589793);
         }
     }
 
-    void LowPassFilter::update(const float dt, float& input_l, float& input_r)
+    void LowPassFilter::update(double dt, float& input_l, float& input_r)
     {
         const float feedback = (resonance + resonance / (1.0f - cutoff));
-        const float two_pi_fc = 2.0f * 3.141592653589f * cutoff * dt;
+        const float two_pi_fc = 2.0f * 3.141592653589f * cutoff * static_cast<float>(dt);
         const float a = two_pi_fc / (two_pi_fc + 1);
 
         // Left channel
--- SoundfontStudies/envs_lfos.h
+++ SoundfontStudies/envs_lfos.h
@@ -3,15 +3,15 @@
 
 namespace Flan {
     struct EnvParams {
-        float delay = 1000.0f;             // Delay in 1.0 / seconds
-        float attack = 1000.0f;	          // Attack in 1.0 / seconds
-        float hold = 1000.0f;		      // Hold in 1.0 / seconds
-        float decay = 0.0f;		      // Decay in dB per second
-        float sustain = 0.0f;		      // Sustain volume in dB (where a value of -6 dB means 0.5x volume, and -12 dB means 0.25x volume)
-        float release = 1000.0f;		      // Release in dB per second
+        double delay = 1000.0;      // Delay in 1.0 / seconds
+        double attack = 1000.0;	    // Attack in 1.0 / seconds
+        double hold = 1000.0;	    //Hold in 1.0 / seconds
+        double decay = 0.0;		    // Decay in dB per second
+        double sustain = 0.0;	    //Sustain volume in dB (where a value of -6 dB means 0.5x volume, and -12 dB means 0.25x volume)
+        double release = 1000.0;    // Release in dB per second
     };
 
-    enum envStage : u8
+    enum EnvStage : u8
     {
         delay = 0,// Hold volume at 0.0
         attack,   // Slowly rise from 0.0 to 1.0
@@ -23,27 +23,27 @@ namespace Flan {
     };
 
     struct EnvState {
-        float stage = static_cast<float>(delay);    // Current stage in ADSR. If floored to an integer and casted to ADSRstage, you get the actual ADSRstage as an enum
-        float value = 0.0f;    // Current envelope volume value in dB
-        void update(const EnvParams& env_params, float dt, bool correct_attack_phase);
+        double stage = delay;    // Current stage in ADSR. If floored to an integer and casted to ADSRstage, you get the actual ADSRstage as an enum
+        double value = 0.0;    // Current envelope volume value in dB
+        void update(const EnvParams& env_params, double dt, bool correct_attack_phase);
     };
 
     struct LfoParams {
-        float freq = 8.2f; // Frequency in Hz
-        float delay = 0.0f; // Delay in seconds
+        double freq = 8.2; // Frequency in Hz
+        double delay = 0.0; // Delay in seconds
     };
 
     struct LfoState {
-        float time = 0.0;
-        float state = 0.0f;
-        void update(const LfoParams& lfo_params, float dt);
+        double time = 0.0;
+        double state = 0.0;
+        void update(const LfoParams& lfo_params, double dt);
     };
 
     struct LowPassFilter {
-        float cutoff = 20005.f;
-        float resonance = 0.0f;
-        float state1[2] = { 0.0f, 0.0f };
-        float state2[2] = { 0.0f, 0.0f };
-        void update(float dt, float& input_l, float& input_r);
+        float cutoff = 20005.0;
+        float resonance = 0.0;
+        float state1[2] = { 0.0, 0.0 };
+        float state2[2] = { 0.0, 0.0 };
+        void update(double dt, float& input_l, float& input_r);
     };
 }
